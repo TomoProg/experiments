8f75edda594d4aed72eea7548bf66863771107ce
TomoProg <t.yamamoto@neogenia.co.jp>
2024-01-07 19:12:25 +0900

    activerecord-importのrecursiveオプションを試すテストを追加した

--- rails_activerecord_import_recursive/web/myapp/test/models/order_test.rb
+++ rails_activerecord_import_recursive/web/myapp/test/models/order_test.rb
@@ -1,15 +1,34 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  puts "aaaaa"
-  # test "the truth" do
-  #   assert true
-  # end
-  test "mariadb_接続" do
-    Mariadb::Order.create(user_name: 'mariadb')
+  test "activerecord-import_recursive_mariadb" do
+    orders = []
+    10.times do |i|
+      order = Mariadb::Order.new(user_name: "user_name #{i}")
+      2.times do |j|
+        order.order_details.build(product_name: "product_name #{i}-#{j}")
+      end
+      orders << order
+    end
+    Mariadb::Order.import orders, recursive: true
 
-    Postgres::Order.create(user_name: 'postgres')
-    binding.pry
+    assert_equal 10, Mariadb::Order.count
+    assert_equal 0, Mariadb::OrderDetail.count
+  end
+
+  test "activerecord-import_recursive_postgres" do
+    orders = []
+    10.times do |i|
+      order = Postgres::Order.new(user_name: "user_name #{i}")
+      2.times do |j|
+        order.order_details.build(product_name: "product_name #{i}-#{j}")
+      end
+      orders << order
+    end
+    Postgres::Order.import orders, recursive: true
+
+    assert_equal 10, Postgres::Order.count
+    assert_equal 20, Postgres::OrderDetail.count
   end
 
 end
