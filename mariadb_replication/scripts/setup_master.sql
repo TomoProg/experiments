33ca223e34f2fda671595a96298f69ea10ff4998
TomoProg <t.yamamoto@neogenia.co.jp>
2024-08-14 06:50:32 +0900

    レプリケーション実装コミット漏れ

---
+++ mariadb_replication/scripts/setup_master.sql
@@ -0,0 +1,16 @@
+-- レプリケーション用のユーザを作成する
+-- slaveからはこのユーザで接続する
+create user if not exists 'repl'@'%' identified by '12345678';
+
+-- レプリケーション
+-- slaveがバイナリログイベントを読み込めるようにする
+grant replication slave on *.* to 'repl'@'%';
+
+-- 初期データ作成
+create database if not exists t1;
+use t1;
+create table if not exists t1(id int, name varchar(20));
+insert into t1 values(1, 'taro');
+insert into t1 values(2, 'jiro');
+
+system echo 'Success!!!';
